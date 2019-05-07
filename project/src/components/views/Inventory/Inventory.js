dab943271ee369283790ba263e1f7995d75bacbd
Auram20 <myriamanik97@gmail.com>
2019-05-07 11:29:06 +0200

    Inventory done, delete item to fix

--- project/src/components/views/Inventory/Inventory.js
+++ project/src/components/views/Inventory/Inventory.js
@@ -12,26 +12,15 @@ export default (props) => {
       <Inventory onchange={(e) => actions.change({event: e, what: 'inventory'})} state={state}>Inventory</Inventory>
       <button type="button" onclick={() => actions.addToInvList()}> Ajouter </button>
       <div class="grid">
-        <div class="element" id="firstWeapon">Empty</div>
-        <div class="element" id="secondWeapon">Empty</div>
-        <div class="element" id="armor">Empty</div>
-        <div class="element" id="elt1">My dynamic content</div>
-        <div class="element" id="elt2">My dynamic content 2</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
-        <div class="element">Empty</div>
+        <div class="element" id="firstWeapon">Weapon 1</div>
+        <div class="element" id="secondWeapon">Weapon 2</div>
+        <div class="element" id="armor"> Armor </div>
+        {
+          state.InventoryList.map(inv => <div id={inv.index} class="element" id={inv.id} content={inv.content}>
+            {inv.content}
+            <a class="deleteLink"onclick={() => actions.deleteToInvList(props.id)}>Delete</a>
+          </div>)
+        }
       </div>
       <p>Weight : <span>0</span></p>
     </div>
